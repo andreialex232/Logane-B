28b96c4c4ca75ae4c738a38b07cdbf069879eaa7
andreialex232 <oricumnumauit@gmail.com>
2024-01-23 21:39:13 +0200

    Features section

--- src/app/home/features/features.component.ts
+++ src/app/home/features/features.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class FeaturesComponent {
 
+  features: any[] = [
+    { heading: 'Contracte', text: 'Incepi sa castigi imediat ce actele au fost emise. Flota noastră colaborează doar cu PFA , II , IF SRL etc. Noi va putem ajuta, indruma sau chiar obtine documentatia necesara deschiderii unui PFA.'},
+    { heading: 'Cand poti sa incepi', text: 'Ca sofer de transport alternativ in Romania incepi sa castigi imediat ce actele au fost emise. Flota noastra activeaza deja in 4 orase din Romania: Bucuresti, Pitesti , Buzau si Constanta, urmand sa activam si in restul oraselor din tara.' },
+    { heading: 'Venituri', text: 'Pentru aceasta activitate veniturile depind doar de timpul alocat de catre dumneavoastra pentru aceasta activitate. Nu aveți limitat și nici restricții din partea noastră. Un program de lucru normal , iti va aduce venituri considerabile . Conduceti cat doriti.' },
+    { heading: 'Marime', text: 'Flota noastra este autorizata de catre Autoritatea Rutiera Romana ca operator de transport alternativ ,conform reglementarilor in vigoare. Administram conturile soferilor parteneri si oferim suport permanent pentru activitatea de transport alternativ .' },
+    { heading: 'Prezenti peste tot', text: 'Suntem prezenti in Bucuresti , Buzau , Pitesti si Constanta. In viitor planuim deschiderea si in restul oraselor din Romania , unde serviciul de Ride-Sharing este disponibil astfel incat sa oferim servicii de calitate indiferent de orasul in care locuiesti.' },
+    { heading: 'Invita si castiga', text: 'Prezinta flota noastra prietenilor si castiga 200 de lei ! Acest bonus se acorda fiecarui sofer nou si nu exista un numar limitat de recomandari , poti aduce cate persoane noi vrei .' }
+  ];
 }
