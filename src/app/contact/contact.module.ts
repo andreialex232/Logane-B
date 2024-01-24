72dea1818b8fc890640237b6c3688b6d30f0d28d
andreialex232 <oricumnumauit@gmail.com>
2024-01-24 20:32:59 +0200

    Generated about and info with routing module and contact component without routing

---
+++ src/app/contact/contact.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactHomeComponent } from './contact-home/contact-home.component';
+
+
+
+@NgModule({
+  declarations: [
+    ContactHomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContactModule { }
---
+++ src/app/info/info.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InfoRoutingModule } from './info-routing.module';
+import { InfoHomeComponent } from './info-home/info-home.component';
+
+
+@NgModule({
+  declarations: [
+    InfoHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    InfoRoutingModule
+  ]
+})
+export class InfoModule { }
