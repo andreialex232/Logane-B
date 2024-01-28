ce420f3f0060f1cd9bc2858a8c8979d96e15caf2
andreialex232 <oricumnumauit@gmail.com>
2024-01-28 02:50:31 +0200

    Form validation

--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ElementRef } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
   standalone: true,
+  imports: [ReactiveFormsModule, CommonModule]
 })
 export class FormComponent {
 
+  public form: FormGroup;
+  public attemptSubmit = false;
+
+  constructor(private fb: FormBuilder, private elRef: ElementRef){
+    this.form = this.fb.group({
+      name: new FormControl('', [Validators.required, Validators.pattern(/[a-z]/gi)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      tel: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]\d*$/)])
+    })
+  }
+
+  submitFrom(){
+    this.attemptSubmit = true;
+
+    if(this.form.valid){
+      console.log('valid')
+    } else {
+      console.log('invalid')
+    }
+  }
 }
