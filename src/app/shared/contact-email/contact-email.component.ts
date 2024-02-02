a7cf5ab7a7f9abbf80959e565f6ae6c950aacda9
andreialex232 <oricumnumauit@gmail.com>
2024-02-02 16:25:27 +0200

    Form, Form Modal and Contact responsive

--- src/app/shared/contact-email/contact-email.component.ts
+++ src/app/shared/contact-email/contact-email.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, inject } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormComponent } from '../form/form.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact-email',
   templateUrl: './contact-email.component.html',
   styleUrls: ['./contact-email.component.scss'],
   standalone: true,
+  imports: [FormComponent, RouterLink]
 })
 export class ContactEmailComponent {
-
+  private modalService = inject(NgbModal);
+  
+	openFormModal(content: TemplateRef<any>) {
+		this.modalService.open(content, { centered: true });
+	}
 }
