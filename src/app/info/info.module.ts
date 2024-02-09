3ea357b249f7fabb6460821bb993e5cf576142c3
andreialex232 <oricumnumauit@gmail.com>
2024-02-09 23:18:49 +0200

    Few changes to dropdown and organised dropdown menu

--- src/app/info/info.module.ts
+++ src/app/info/info.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 import { InfoRoutingModule } from './info-routing.module';
 import { InfoHomeComponent } from './info-home/info-home.component';
