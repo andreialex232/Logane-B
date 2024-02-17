7601905cdb7a493ff02cd07d64b1251ca499d632
andreialex232 <oricumnumauit@gmail.com>
2024-02-17 21:39:57 +0200

    Hamburger menu second prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { InfoModule } from './info/info.module';
 import { AboutModule } from './about/about.module';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,6 +21,7 @@ import { AboutModule } from './about/about.module';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
