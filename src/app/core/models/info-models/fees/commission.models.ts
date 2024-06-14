a14fed262311de6b0aa9344fd186e436ec99e2af
andreialex232 <numauitnuintelegi@gmail.com>
2024-06-14 17:22:08 +0300

    updated fees

--- src/app/core/models/info-models/fees/commission.models.ts
+++ src/app/core/models/info-models/fees/commission.models.ts
@@ -2,12 +2,12 @@ import { ITextList } from "src/app/core/interfaces/plain-text.models";
 
 export const COMMISSION: ITextList[] = [
   {
-    text: '8% - pentru veniturile între 0 - 2500 Ron'
+    text: '10% - pentru veniturile între 0 - 5000 Ron'
   },
   {
-    text: '7% - pentru veniturile între 2500 - 5000 Ron'
+    text: '9% - pentru veniturile între 5000 - 10000 Ron'
   },
   {
-    text: '5% - pentru veniturile de peste 5000 Ron'
+    text: '8% - pentru veniturile de peste 10000 Ron'
   }
 ];
